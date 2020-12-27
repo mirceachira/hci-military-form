9cc7979e9e5fd54c31d8e797c067d53b10261071
mirceachira <chira.mircea.darius@gmail.com>
2020-12-27 13:54:44 +0200

    added black

--- militaryform/urls.py
+++ militaryform/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('chestionar/', include('survey.urls')),
+    path("admin/", admin.site.urls),
+    path("chestionar/", include("survey.urls")),
 ]
